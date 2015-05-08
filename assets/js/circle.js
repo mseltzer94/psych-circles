4d75f12a7dc25822d328772eb197227d0137b09e
mseltzer94 <michael.m.seltzer@gmail.com>
2015-05-08 00:11:58 -0400

    add circles to chart based on array size

--- assets/js/circle.js
+++ assets/js/circle.js
@@ -1,4 +1,11 @@
+circles = ['race', 'gender'];
+
 $(document).ready( function() {
-  var $draggable =$('.draggable').draggabilly({
+  for (i = 0; i < circles.length; i++) {
+    $('.container').append('<div class="draggable"><div class=title>'+ circles[i]+'</div></div>');
+  }
+    
+    var $draggable =$('.draggable').draggabilly({
+    containment: true
   });
 });
