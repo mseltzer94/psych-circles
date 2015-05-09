9ae2be066085a61c83ec08fbc10bfe2ae8f6d9db
mseltzer94 <michael.m.seltzer@gmail.com>
2015-05-09 12:39:50 -0400

    change datatype of circles array

--- assets/js/circle.js
+++ assets/js/circle.js
@@ -1,15 +1,39 @@
-partid = '';
-circles = ['race', 'gender'];
+//array shuffle procedure from //http://www.kirupa.com/html5/shuffling_array_js.htm
+Array.prototype.shuffle = function() {
+    var input = this;
+     
+    for (var i = input.length-1; i >=0; i--) {
+     
+        var randomIndex = Math.floor(Math.random()*(i+1)); 
+        var itemAtIndex = input[randomIndex]; 
+         
+        input[randomIndex] = input[i]; 
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
 
+partid = '';
+circles = [
+    {"name":'Race', "question":"Enter a race: "},
+    {"name":'Gender', "question":""}, 
+    {"name":'Age', "question":""}];
+
+circles.shuffle();
 $(document).ready( function() {
   for (i = 0; i < circles.length; i++) {
-    $('.container').append('<div class="draggable"><div class=title>'+ circles[i]+'</div></div>');
+    $('.container').append('<div class="draggable"><div class=title>'+ circles[i].name+'</div></div>');
   }
     
     var $draggable =$('.draggable').draggabilly({
     containment: true
   });
+$('.draggable').on( 'dragEnd', 
+              function( event, pointer ) {
+    alert('hello');
+    });
 });
+
 
 function submit() {
     
